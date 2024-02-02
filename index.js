c23dbe4a4114cd1e8f4af3e21b1205a3d1948522
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-02-02 21:56:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,140 @@
+class node {
+  constructor() {
+    this.value = null;
+    this.nextNode = null;
+  }
+}
+class linkedList {
+  constructor() {
+    this.listHead = null;
+  }
+  append(value) {
+    if (this.listHead == null) this.prepend(value);
+    else {
+      let temp = this.listHead;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = new node();
+      temp.nextNode.value = value;
+    }
+  }
+  prepend(value) {
+    if (this.listHead == null) {
+      this.listHead = new node();
+      this.listHead.value = value;
+    } else {
+      let temp = this.listHead;
+      this.listHead = new node();
+      this.listHead.value = value;
+      this.listHead.nextNode = temp;
+    }
+  }
+  head() {
+    if (this.listHead == null) {
+      return "no nodes";
+    } else return this.listHead;
+  }
+  tail() {
+    if (this.listHead == null) {
+      return "no nodes";
+    } else {
+      let temp = this.listHead;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode;
+      }
+      return temp;
+    }
+  }
+  size() {
+    if (this.listHead == null) {
+      return 0;
+    } else {
+      let temp = this.listHead;
+      let nodeNumber = 1;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode;
+        nodeNumber++;
+      }
+      return nodeNumber;
+    }
+  }
+  at(index) {
+    if (index == 0) return this.listHead;
+    else {
+      let temp = this.listHead;
+      let nodeNumber = 1;
+      while (temp.nextNode != null) {
+        if (index == nodeNumber) {
+          return temp.nextNode;
+        }
+        temp = temp.nextNode;
+        nodeNumber++;
+      }
+      return "not enough nodes";
+    }
+  }
+  pop() {
+    if (this.listHead == null) "no nodes to pop";
+    else {
+      let temp = this.listHead;
+      while (temp.nextNode != null) {
+        if (temp.nextNode.nextNode == null) {
+          temp.nextNode = null;
+          return;
+        }
+        temp = temp.nextNode;
+      }
+    }
+  }
+  contains(value) {
+    if (this.listHead == null) {
+      return "no nodes";
+    } else {
+      let temp = this.listHead;
+      while (temp != null) {
+        if (temp.value == value) {
+          return true;
+        }
+        temp = temp.nextNode;
+      }
+      return false;
+    }
+  }
+  find(value) {
+    if (this.listHead.value == value) return 0;
+    else {
+      let temp = this.listHead;
+      let nodeNumber = 0;
+      while (temp != null) {
+        if (temp.value == value) {
+          return nodeNumber;
+        }
+        temp = temp.nextNode;
+        nodeNumber++;
+      }
+      return null;
+    }
+  }
+  toString() {
+    let temp = this.listHead;
+    let string = "";
+    if (temp == null) {
+      return null;
+    }
+    while (temp != null) {
+      string = string.concat(` ${temp.value} => `);
+      temp = temp.nextNode;
+    }
+    string = string.concat("null");
+    return string;
+  }
+}
+
+const list = new linkedList();
+list.append("poop");
+list.append("poop2");
+list.append("poop3");
+list.prepend("poop0");
+console.log(list.toString());
+console.log(list);
